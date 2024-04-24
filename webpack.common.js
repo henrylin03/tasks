ef63ba4ada107fd005a1c4e31b376b3dd06c52f8
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-04-24 16:11:51 +1000

    split config files for dev and for prod

--- webpack.common.js
+++ webpack.common.js
@@ -2,14 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
   entry: "./src/index.js",
-  devtool: "inline-source-map",
-  devServer: {
-    static: "./dist",
-    open: true,
-    watchFiles: ["src/**/*"],
-  },
   module: {
     rules: [
       {
