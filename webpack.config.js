802e9cf57a96a73ca659fe1b2d1ace925d25c241
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-04-24 14:04:18 +1000

    set up environment for development, and add devtool to source map errors

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
@@ -20,6 +21,7 @@ module.exports = {
       },
     ],
   },
+  devtool: "inline-source-map",
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
