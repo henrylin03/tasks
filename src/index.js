83cc70c306148496624d4357888ccb99dfbc35a1
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-04-29 11:21:24 +1000

    create a tasklist, which is just the global environment for all to-dos. add ability to have to dos be added to the tasklist, and have the task list be printed

--- src/index.js
+++ src/index.js
@@ -1,4 +1,32 @@
 import createToDo from "./createToDo";
 
+// a task list should be an array of objects, each object representing a todo
+const createTaskList = () => {
+  const taskList = [];
+  const add = (toDoObject) => {
+    taskList.push(toDoObject);
+  };
+
+  const print = () => {
+    console.log("Your tasks are below:");
+    taskList.forEach((t) => console.log(t));
+  };
+
+  return { add, print };
+};
+
 const toDo1 = createToDo("Get your shit together");
-console.log(toDo1);
+const toDo2 = createToDo(
+  "Work hard",
+  "Do something you love and it won't really feel like work at all"
+);
+const toDo3 = createToDo(
+  "Relax with your family",
+  "Nothing else has as much meaning"
+);
+
+const taskList = createTaskList();
+taskList.add(toDo1);
+taskList.add(toDo2);
+taskList.add(toDo3);
+taskList.print();
