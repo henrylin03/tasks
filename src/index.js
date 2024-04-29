b86cb3b7d74d188f0400e114c0909893f7a2b952
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-04-29 16:03:12 +1000

    create iife function that test alerts 'hello world'
    this purpose of this function is to act as the 'main' function. the plan is to have this function seek user input and display results in the console. this will help nut out the detailed business logic before we later move onto ui dom manipulation

--- src/index.js
+++ src/index.js
@@ -2,15 +2,20 @@ import createTask from "./createTask";
 import createTaskList from "./createTaskList";
 
 // instantiating/testing
-const taskList = createTaskList();
-taskList.addTask(createTask("Get your shit together"));
-taskList.addTask(
-  createTask(
-    "Work hard",
-    "Do something you love and it won't really feel like work at all"
-  )
-);
-taskList.addTask(
-  createTask("Relax with your family", "Nothing else has as much meaning")
-);
-taskList.printTask();
+// const taskList = createTaskList();
+// taskList.addTask(createTask("Get your shit together"));
+// taskList.addTask(
+//   createTask(
+//     "Work hard",
+//     "Do something you love and it won't really feel like work at all"
+//   )
+// );
+// taskList.addTask(
+//   createTask("Relax with your family", "Nothing else has as much meaning")
+// );
+// taskList.printTask();
+
+const taskManager = (function () {
+  alert("hello world");
+  return;
+})();
