999c4e70e45b0ff7c44aaa3eb4a8866f428f5c91
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-04-29 11:48:45 +1000

    simplify instantiations
    there is no utility for the actual variable names for the to do items at the moment

--- src/index.js
+++ src/index.js
@@ -2,18 +2,15 @@ import createToDo from "./createToDo";
 import createTaskList from "./createTaskList";
 
 // instantiating/testing
-const toDo1 = createToDo("Get your shit together");
-const toDo2 = createToDo(
-  "Work hard",
-  "Do something you love and it won't really feel like work at all"
-);
-const toDo3 = createToDo(
-  "Relax with your family",
-  "Nothing else has as much meaning"
-);
-
 const taskList = createTaskList();
-taskList.add(toDo1);
-taskList.add(toDo2);
-taskList.add(toDo3);
+taskList.add(createToDo("Get your shit together"));
+taskList.add(
+  createToDo(
+    "Work hard",
+    "Do something you love and it won't really feel like work at all"
+  )
+);
+taskList.add(
+  createToDo("Relax with your family", "Nothing else has as much meaning")
+);
 taskList.print();
