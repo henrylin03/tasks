61398a1084784a4bebbf7a6608ff9fef8af432a7
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-04-29 16:10:44 +1000

    add test tasks to the tasklist
    this is to test other functionality before we introduce localStorage

--- src/index.js
+++ src/index.js
@@ -1,25 +1,22 @@
 import createTask from "./createTask";
 import createTaskList from "./createTaskList";
 
-// instantiating/testing
-// const taskList = createTaskList();
-// taskList.addTask(createTask("Get your shit together"));
-// taskList.addTask(
-//   createTask(
-//     "Work hard",
-//     "Do something you love and it won't really feel like work at all"
-//   )
-// );
-// taskList.addTask(
-//   createTask("Relax with your family", "Nothing else has as much meaning")
-// );
-// taskList.printTask();
-
 const taskManager = (function () {
+  const taskList = createTaskList();
+
   console.log(" ~ welcome to serenity to-dos ~");
 
-  const taskList = createTaskList();
-  taskList.printTasks();
+  // adding a bunch of test tasks - these can be deleted when we introduce localStorage management
+  taskList.addTask(createTask("Get your shit together"));
+  taskList.addTask(
+    createTask(
+      "Work hard",
+      "Do something you love and it won't really feel like work at all"
+    )
+  );
+  taskList.addTask(
+    createTask("Relax with your family", "Nothing else has as much meaning")
+  );
 
-  return;
+  taskList.printTasks();
 })();
