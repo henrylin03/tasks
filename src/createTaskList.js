f145d532ba6c9a33ee02239cd37e71b255ac5e99
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-04-29 15:58:01 +1000

    make methods of task list more specific
    they all relate to actual tasks

--- src/createTaskList.js
+++ src/createTaskList.js
@@ -1,15 +1,15 @@
 // a task list should be an array of objects, each object representing a todo
 const createTaskList = () => {
   const taskList = [];
-  const add = (toDoObject) => {
+  const addTask = (toDoObject) => {
     taskList.push(toDoObject);
   };
 
-  const print = () => {
+  const printTask = () => {
     console.log("TO-DOS:", taskList);
   };
 
-  const updateToDo = (index, property, updatedValue) => {
+  const updateTask = (index, property, updatedValue) => {
     if (taskList[index] === undefined)
       throw new Error(
         "That to-do index does not exist. Please select to-do item based on its index in the to do list."
@@ -24,10 +24,10 @@ const createTaskList = () => {
     console.log(`Updating to-do: "${selectedToDo.title}"`);
     taskList[index][property] = updatedValue;
 
-    print();
+    printTask();
   };
 
-  return { add, print, updateToDo };
+  return { addTask, printTask, updateTask };
 };
 
 export default createTaskList;
--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import createTaskList from "./createTaskList";
 
 // instantiating/testing
 const taskList = createTaskList();
-taskList.add(createTask("Get your shit together"));
-taskList.add(
+taskList.addTask(createTask("Get your shit together"));
+taskList.addTask(
   createTask(
     "Work hard",
     "Do something you love and it won't really feel like work at all"
   )
 );
-taskList.add(
+taskList.addTask(
   createTask("Relax with your family", "Nothing else has as much meaning")
 );
-taskList.print();
+taskList.printTask();
