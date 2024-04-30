90d972c14cd6c4890975523cebe8a6f78c39d7ce
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-04-30 17:01:03 +1000

    add methods to add tasks to project, view tasks in project, remove task from project. rename name of project as 'name' to avoid 'title' of the task - avoids name clashing
    the thinking i have at the moment is that the list of tasks (all tasks, regardless of project) is either an object where keys are the project name, values are arrays of task objects OR a flat array of tasks where the task.project attribute is the project object. i'm not sure which one is better so i'm gonna take a stab at both and see which one ends up pushed to main branch here

--- src/createProject.js
+++ src/createProject.js
@@ -1,5 +1,22 @@
-const createProject = (title) => {
-  return { title };
+const createProject = (name) => {
+  const tasksInProject = [];
+
+  const addTaskToProject = (task) => tasksInProject.push(task);
+
+  const viewTasksInProject = () => {
+    console.log(
+      `The following are the tasks in the project "${name}": [${tasksInProject}]`
+    );
+  };
+
+  const removeTaskFromProject = (taskIndex) => {
+    if (tasksInProject[taskIndex] === undefined)
+      throw new Error("Index does not exist in tasks");
+    tasksInProject.splice(taskIndex, 1);
+    viewTasksInProject();
+  };
+
+  return { name, addTaskToProject, viewTasksInProject, removeTaskFromProject };
 };
 
 export default createProject;
