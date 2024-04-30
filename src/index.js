de3672d233fcae68fd416044e1aa84fd954bc391
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-05-01 09:53:40 +1000

    request input for title, description, dueDate, priority and status

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ const createTask = ({
   description = "",
   dueDate = "",
   priority = "Medium",
+  status = "To Do",
 }) => {
-  return { title, description, dueDate, priority };
+  return { title, description, dueDate, priority, status };
 };
 
 const app = (function () {
@@ -15,4 +16,26 @@ const app = (function () {
     2: modify existing task, 
     3: modify existing project`
   );
+
+  if (input === "1") {
+    input = prompt(`What would you like to add?
+    1: add task,
+    2: add project`);
+
+    if (input === "1") {
+      const title = prompt("Please enter a title for the task");
+      const description = prompt(
+        "Please enter a description for the task. This is optional. Press enter to continue."
+      );
+      const dueDate = prompt(
+        "Please enter a due date for this task. This is optional. Press enter to continue."
+      );
+      const priority = prompt(
+        "Please enter a priority for this task. This is optional. Priority will default to 'Medium'. Press enter to continue."
+      );
+      const status = prompt(
+        "Please enter the status of this task - 'To Do', 'Doing' or 'Done'. Status will default to 'To Do' if none is entered. Press enter to continue."
+      );
+    }
+  }
 })();
