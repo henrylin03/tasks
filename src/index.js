8ef18f5f68640d46c5e762d3391da28bafca2dc8
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-05-01 09:17:19 +1000

    add prompt for first step of asking what user wants to do, add basic factory for task with default parameters for 3 optional properties and 1 required property - the title of the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+const createTask = ({
+  title,
+  description = "",
+  dueDate = "",
+  priority = "Medium",
+}) => {
+  return { title, description, dueDate, priority };
+};
+
 const app = (function () {
-  // ask user if they would like to start a new project
+  // show all tasks from localStorage. if none, show "no tasks yet"
+  let input = prompt(
+    `What would you like to do? Please enter number: 
+    1: add something new, 
+    2: modify existing task, 
+    3: modify existing project`
+  );
 })();
