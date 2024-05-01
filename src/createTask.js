bca7e7c0ab7f64d7bc28e3bfb3157e92e0cb8c0b
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-05-01 10:31:52 +1000

    fix: use let to declare default variables rather than const
    otherwise i would not be able to reassign them using the users' inputs

--- src/createTask.js
+++ src/createTask.js
@@ -2,9 +2,9 @@ import { format } from "date-fns";
 
 const createTask = ({ title, description = "" }) => {
   // default values of private properties
-  const dueDate = "";
-  const priority = "Medium";
-  const status = "To Do";
+  let dueDate = "";
+  let priority = "Medium";
+  let status = "To Do";
 
   const getTitle = () => title;
   const getDescription = () => description;
@@ -14,16 +14,23 @@ const createTask = ({ title, description = "" }) => {
 
   const setTitle = (newTitle) => (title = newTitle);
   const setDescription = (newDescription) => (description = newDescription);
-  const setDueDate = (year, month, date) =>
-    (dueDate = format(new Date(year, month, date), "dd/MM/yyyy"));
+  const setDueDate = (year, month, date) => {
+    dueDate = dueDate
+      ? format(new Date(year, month, date), "dd/MM/yyyy")
+      : dueDate;
+  };
   const setPriority = (newPriority) => {
     const VALID_PRIORITIES = ["Low", "Medium", "High", "Urgent"];
+
+    // ? perhaps shouldn't be an error. just log it and continue.
     if (!VALID_PRIORITIES.includes(newPriority))
       throw new Error("Invalid priority. Defaulting to 'Medium'.");
     priority = newPriority;
   };
   const setStatus = (newStatus) => {
     const VALID_STATUSES = ["To Do", "Doing", "Done", "Won't Do"];
+
+    // ? perhaps shouldn't be an error. just log it and continue.
     if (!VALID_STATUSES.includes(newStatus))
       throw new Error("Invalid status. Defaulting to 'To do'");
     status = newStatus;
