3d4929f5863020c61fd735a547def2719a590e46
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-05-01 11:21:11 +1000

    add guard clauses for status and priority setters in task factory for when user does not enter anything

--- src/createTask.js
+++ src/createTask.js
@@ -19,18 +19,16 @@ const createTask = ({ title, description = "" }) => {
     const [date, month, year] = dueDateString.split("/");
     dueDate = format(new Date(year, month, date), "dd/MM/yyyy");
   };
-
-  //todo: don't show error if user enters nothing
   const setPriority = (newPriority) => {
+    if (!newPriority) return;
     const VALID_PRIORITIES = ["Low", "Medium", "High", "Urgent"];
     if (VALID_PRIORITIES.includes(newPriority)) return (priority = newPriority);
     console.log(
       "Invalid priority. Reverting to the last defined priority or, if this is a new task, defaulting to 'Medium' priority."
     );
   };
-
-  //todo: don't show error if user enters nothing
   const setStatus = (newStatus) => {
+    if (!newStatus) return;
     const VALID_STATUSES = ["To Do", "Doing", "Done", "Won't Do"];
     if (VALID_STATUSES.includes(newStatus)) return (status = newStatus);
     console.log(
