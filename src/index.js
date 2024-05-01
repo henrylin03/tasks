1c74439f29ca571ae93d9b0def82f364edec4775
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-05-01 10:26:11 +1000

    separate out task cfactory as a module and reimported into index.js

--- src/index.js
+++ src/index.js
@@ -1,47 +1,4 @@
-import { format } from "date-fns";
-
-const createTask = ({ title, description = "" }) => {
-  // default values of private properties
-  const dueDate = "";
-  const priority = "Medium";
-  const status = "To Do";
-
-  const getTitle = () => title;
-  const getDescription = () => description;
-  const getDueDate = () => dueDate;
-  const getPriority = () => priority;
-  const getStatus = () => status;
-
-  const setTitle = (newTitle) => (title = newTitle);
-  const setDescription = (newDescription) => (description = newDescription);
-  const setDueDate = (year, month, date) =>
-    (dueDate = format(new Date(year, month, date), "dd/MM/yyyy"));
-  const setPriority = (newPriority) => {
-    const VALID_PRIORITIES = ["Low", "Medium", "High", "Urgent"];
-    if (!VALID_PRIORITIES.includes(newPriority))
-      throw new Error("Invalid priority. Defaulting to 'Medium'.");
-    priority = newPriority;
-  };
-  const setStatus = (newStatus) => {
-    const VALID_STATUSES = ["To Do", "Doing", "Done", "Won't Do"];
-    if (!VALID_STATUSES.includes(newStatus))
-      throw new Error("Invalid status. Defaulting to 'To do'");
-    status = newStatus;
-  };
-
-  return {
-    getTitle,
-    getDescription,
-    getDueDate,
-    getPriority,
-    getStatus,
-    setTitle,
-    setDescription,
-    setPriority,
-    setDueDate,
-    setStatus,
-  };
-};
+import createTask from "./createTask";
 
 const app = (function () {
   // show all tasks from localStorage. if none, show "no tasks yet"
