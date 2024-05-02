e6ac407962d7820ba1b418f0aa0800a0dae45663
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-05-02 16:09:28 +1000

    remove comments

--- src/utils/localStorageHelpers.js
+++ src/utils/localStorageHelpers.js
@@ -1,7 +1,3 @@
-/*
-in our localStorage, we can use .getItem(projectId) to return the project object (//? but what about the methods? they need to be recreated it seems...)
-*/
-
 const storeProject = (projectObject) => {
   const projectIsNew = projectObject.hasOwnProperty("viewDetails");
   const projectDetails = projectIsNew
@@ -23,5 +19,3 @@ const retrieveAll = () => {
 };
 
 export { retrieveAll, retrieveProject, storeProject };
-
-//todo: in order for your objects to have methods (setters, getters etc), it needs to be recreated using factory after you bring it back. look at: https://stackoverflow.com/questions/64141609/saving-objects-in-localstorage-which-has-a-method
