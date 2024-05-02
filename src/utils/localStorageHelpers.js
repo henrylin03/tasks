9cd2acb9d900612caa2eba007ab300185b7a569e
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-05-02 13:45:11 +1000

    add comments

--- src/utils/localStorageHelpers.js
+++ src/utils/localStorageHelpers.js
@@ -1,13 +1,11 @@
 /*
-in our localStorage, we can use .getItem(projectId) to return the project object
+in our localStorage, we can use .getItem(projectId) to return the project object (//? but what about the methods? they need to be recreated it seems...)
 
 // in our localStorage, we will have two "objects".
 
 // -object #1 ("projectIdToTasks"): key = unique id of project, value = array of objects representing the projects' tasks (as objects)
 // -object #2 ("projectIdToProjects"): lookup for project where key = unique id of project, value = project object
 */
-
-const noProjectsCreated = localStorage.length === 0;
 
 const getAllStoredTasks = () => ({ ...localStorage });
 
@@ -24,4 +22,6 @@ const storeProject = (projectObject) => {
 //   );
 // };
 
-export { getAllStoredTasks, storeProject, noProjectsCreated };
+export { getAllStoredTasks, storeProject };
+
+//todo: in order for your objects to have methods (setters, getters etc), it needs to be recreated using factory after you bring it back. look at: https://stackoverflow.com/questions/64141609/saving-objects-in-localstorage-which-has-a-method
