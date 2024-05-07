c043294205da07ec228c1fe5e216fa7644d66615
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-05-07 10:37:14 +1000

    when tasks are being added to a project, if the tasks' details already exists, it is not added again

--- src/factories/project.js
+++ src/factories/project.js
@@ -20,8 +20,11 @@ const createProject = (name) => {
   const addTasks = (...taskObjects) =>
     // we do not need the tasks' methods, so we only add tasks' details to project
     taskObjects.forEach((task) => {
+      // ? we may wish to ensure all tasks are also reconstructed to avoid manipulating properties that were intended to be private upon creation, just because we've retrieved from localStorage
+
       const isReconstructedTask = task.hasOwnProperty("viewDetails");
       const taskDetails = isReconstructedTask ? task.viewDetails() : task;
+      if (tasks.includes(taskDetails)) return;
       tasks.push(taskDetails);
     });
 
@@ -42,3 +45,5 @@ export { createProject, createProjectFromJSON };
 // todo: align pattern w/ task for a separate factory to recreate project object with all of its methods from retrieved json. explore benefits of having a setter object (maybe not here)
 
 //todo: once we have cerate project factory from json, adjust localstorage helpers - it should reduce linecount there
+
+//TODO: deduplicate tasks within a project
