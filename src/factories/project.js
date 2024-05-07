bbd16bb87e362639d8da0513ea12ae4c2212a799
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-05-07 10:27:37 +1000

    add 1+ task objects to task list of a project. checking if task has been reconstructed or not first
    otherwise, we would be calling the .viewDetails() method on an object that only has strings, as it has been .getItem()'d from localStorage

--- src/factories/project.js
+++ src/factories/project.js
@@ -16,29 +16,29 @@ const createProject = (name) => {
       );
   };
 
-  const addTaskDetails = (taskObject) => tasks.push(taskObject.viewDetails());
+  // accepts 1+ task objects
+  const addTasks = (...taskObjects) =>
+    // we do not need the tasks' methods, so we only add tasks' details to project
+    taskObjects.forEach((task) => {
+      const isReconstructedTask = task.hasOwnProperty("viewDetails");
+      const taskDetails = isReconstructedTask ? task.viewDetails() : task;
+      tasks.push(taskDetails);
+    });
 
   // run checks
   checkIfProjectNameAlreadyExists(name);
 
-  return { viewDetails, addTaskDetails };
+  return { viewDetails, addTasks };
 };
 
-// const createProjectFromJSON = (retrievedProject) => {
-//   const reconstructedProject = createTask({
-//     title: retrievedProject.name,
-//     description: retrievedProject.description,
-//   });
+const createProjectFromJSON = (retrievedProject) => {
+  const reconstructedProject = createProject(retrievedProject.name);
 
-//   for (const [retrievedProperty, retrievedValue] of Object.entries(
-//     retrievedTask
-//   )) {
-//     reconstructedTask.set[retrievedProperty](retrievedValue);
-//   }
+  return reconstructedProject;
+};
 
-//   return reconstructedTask;
-// };
-
-export { createProject };
+export { createProject, createProjectFromJSON };
 
 // todo: align pattern w/ task for a separate factory to recreate project object with all of its methods from retrieved json. explore benefits of having a setter object (maybe not here)
+
+//todo: once we have cerate project factory from json, adjust localstorage helpers - it should reduce linecount there
