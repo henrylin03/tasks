a75f05c4b5a5d323682b60b7d3b3a078d5b80d2a
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-05-07 16:54:12 +1000

    remove comments

--- src/index.js
+++ src/index.js
@@ -221,4 +221,3 @@ Current projects: [ ${retrieveAllProjectNames()} ]`);
 
 //todo: need to refactor the app here so it's not spaghetti code - package into different functions (methods of an 'app controller')
 //? explore using unique ids of tasks and projects to establish relations rather than manipulating arrays and objects etc... it is quite good to have a unique identifier i think. better performance?
-// ! right now, there might be a potential issue whereby once a project is retrieved, we're not reconstructing it BEFORE just pushing things to its supposedly private properties (eg the tasks assigned to the project). this mixes and matches public / private might become an issue...
