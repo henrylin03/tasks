8e92f31283ca686590ec26c9f2ae82cdcef981fb
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-05-09 09:53:19 +1000

    update title of html page. comment out all of index.js to switch off all of the alerts/prompts in dev

--- src/index.js
+++ src/index.js
@@ -1,223 +1,227 @@
-import { createTask, createTaskFromJSON } from "./factories/task";
-import { createProject, createProjectFromJSON } from "./factories/project";
-import {
-  retrieveAll,
-  retrieveAllProjectNames,
-  retrieveProject,
-  findProjectsWithTask,
-  taskExists,
-  projectExists,
-  retrieveTask,
-  storeTask,
-} from "./utils/localStorageHelpers";
+console.log(
+  "in build of html. switching this js off because of all the alerts / prompts in this pre-ui version."
+);
 
-const app = (function () {
-  // localStorage.clear(); // comment out when you need to reset
-  console.log(retrieveAll());
-  if (localStorage.length === 0) createProject("My Tasks").store();
+// import { createTask, createTaskFromJSON } from "./factories/task";
+// import { createProject, createProjectFromJSON } from "./factories/project";
+// import {
+//   retrieveAll,
+//   retrieveAllProjectNames,
+//   retrieveProject,
+//   findProjectsWithTask,
+//   taskExists,
+//   projectExists,
+//   retrieveTask,
+//   storeTask,
+// } from "./utils/localStorageHelpers";
 
-  let input = prompt(
-    `What would you like to do? Please enter number: 
-    1: add something new, 
-    2: modify existing task, 
-    3: modify existing project`
-  );
+// const app = (function () {
+//   // localStorage.clear(); // comment out when you need to reset
+//   console.log(retrieveAll());
+//   if (localStorage.length === 0) createProject("My Tasks").store();
 
-  if (input === "1") {
-    input = prompt(`What would you like to add?
-    1: add task,
-    2: add project`);
+//   let input = prompt(
+//     `What would you like to do? Please enter number:
+//     1: add something new,
+//     2: modify existing task,
+//     3: modify existing project`
+//   );
 
-    if (input === "1") {
-      const newTask = createTask({
-        title: prompt("Please enter a title for the task"),
-        description: prompt("Please enter the description of this task"),
-      });
-      newTask.set.dueDate(
-        prompt(
-          "Please enter a due date for this task in the format: `dd/mm/yyyy` (eg 1/1/2025 for 2025 New Years Day). This is optional. Press enter to not include a due date."
-        )
-      );
-      newTask.set.priority(
-        prompt(
-          "Please enter a priority for this task. This is optional. Priority will default to 'Medium'. Press enter to continue."
-        )
-      );
-      newTask.set.status(
-        prompt(
-          "Please enter the status of this task - 'To Do', 'Doing' or 'Done'. Status will default to 'To Do' if none is entered. Press enter to continue."
-        )
-      );
+//   if (input === "1") {
+//     input = prompt(`What would you like to add?
+//     1: add task,
+//     2: add project`);
 
-      input = prompt(`Would you like to tag this to a project? (y / n)`);
-      if (input === "y") {
-        input = prompt(
-          `Would you like to add this to an existing project? (y / n)`
-        );
-        if (input === "y") {
-          while (true) {
-            const selectedProjectName =
-              prompt(`Please type the name of the project to add this task to:
+//     if (input === "1") {
+//       const newTask = createTask({
+//         title: prompt("Please enter a title for the task"),
+//         description: prompt("Please enter the description of this task"),
+//       });
+//       newTask.set.dueDate(
+//         prompt(
+//           "Please enter a due date for this task in the format: `dd/mm/yyyy` (eg 1/1/2025 for 2025 New Years Day). This is optional. Press enter to not include a due date."
+//         )
+//       );
+//       newTask.set.priority(
+//         prompt(
+//           "Please enter a priority for this task. This is optional. Priority will default to 'Medium'. Press enter to continue."
+//         )
+//       );
+//       newTask.set.status(
+//         prompt(
+//           "Please enter the status of this task - 'To Do', 'Doing' or 'Done'. Status will default to 'To Do' if none is entered. Press enter to continue."
+//         )
+//       );
 
-Current projects: [ ${retrieveAllProjectNames()} ]`);
-            if (!projectExists(selectedProjectName)) {
-              alert("Project does not exist. Please check your project name.");
-              continue;
-            }
-            const retrievedProject = createProjectFromJSON(
-              retrieveProject(selectedProjectName)
-            );
-            retrievedProject.addTasks(newTask);
-            retrievedProject.store();
-            break;
-          }
-        } else if (input === "n") {
-          const newProject = createProject(
-            prompt("Please enter a name for your new project")
-          );
-          newProject.addTasks(newTask);
-          newProject.store();
-        }
-      } else if (input === "n") {
-        const myTasksProject = createProjectFromJSON(
-          retrieveProject("My Tasks")
-        );
-        myTasksProject.addTasks(newTask);
-        myTasksProject.store();
-      }
-    } else if (input === "2") {
-      const newProject = createProject(
-        prompt("Please enter a name for your project")
-      );
-      newProject.store();
-    }
-  } else if (input === "2") {
-    const selectedTaskName = prompt(
-      "Please enter the name of the task you would like to modify:"
-    );
-    if (!taskExists(selectedTaskName))
-      throw new Error(`Task name, "${taskName}", does not exist.`);
+//       input = prompt(`Would you like to tag this to a project? (y / n)`);
+//       if (input === "y") {
+//         input = prompt(
+//           `Would you like to add this to an existing project? (y / n)`
+//         );
+//         if (input === "y") {
+//           while (true) {
+//             const selectedProjectName =
+//               prompt(`Please type the name of the project to add this task to:
 
-    const retrievedTaskObject = retrieveTask(selectedTaskName);
-    const reconstructedTask = createTaskFromJSON(retrievedTaskObject);
-    const matchedProjects = findProjectsWithTask(selectedTaskName);
+// Current projects: [ ${retrieveAllProjectNames()} ]`);
+//             if (!projectExists(selectedProjectName)) {
+//               alert("Project does not exist. Please check your project name.");
+//               continue;
+//             }
+//             const retrievedProject = createProjectFromJSON(
+//               retrieveProject(selectedProjectName)
+//             );
+//             retrievedProject.addTasks(newTask);
+//             retrievedProject.store();
+//             break;
+//           }
+//         } else if (input === "n") {
+//           const newProject = createProject(
+//             prompt("Please enter a name for your new project")
+//           );
+//           newProject.addTasks(newTask);
+//           newProject.store();
+//         }
+//       } else if (input === "n") {
+//         const myTasksProject = createProjectFromJSON(
+//           retrieveProject("My Tasks")
+//         );
+//         myTasksProject.addTasks(newTask);
+//         myTasksProject.store();
+//       }
+//     } else if (input === "2") {
+//       const newProject = createProject(
+//         prompt("Please enter a name for your project")
+//       );
+//       newProject.store();
+//     }
+//   } else if (input === "2") {
+//     const selectedTaskName = prompt(
+//       "Please enter the name of the task you would like to modify:"
+//     );
+//     if (!taskExists(selectedTaskName))
+//       throw new Error(`Task name, "${taskName}", does not exist.`);
 
-    input = prompt(`What would you like to do with task, "${selectedTaskName}"?
-    1: modify task attributes,
-    2: move to different project,
-    3: delete task`);
+//     const retrievedTaskObject = retrieveTask(selectedTaskName);
+//     const reconstructedTask = createTaskFromJSON(retrievedTaskObject);
+//     const matchedProjects = findProjectsWithTask(selectedTaskName);
 
-    // modify existing task attributes
-    if (input === "1") {
-      const taskProperties = Object.keys(retrievedTaskObject);
+//     input = prompt(`What would you like to do with task, "${selectedTaskName}"?
+//     1: modify task attributes,
+//     2: move to different project,
+//     3: delete task`);
 
-      const propertyToModify = prompt(
-        "Please enter the property name you would like to change (title, description, dueDate, priority, status): "
-      );
-      if (!taskProperties.includes(propertyToModify))
-        throw new Error(
-          `A task does not have that property. Please select from the following: 
-    title, description, dueDate, priority, status`
-        );
+//     // modify existing task attributes
+//     if (input === "1") {
+//       const taskProperties = Object.keys(retrievedTaskObject);
 
-      const updatedValue = prompt("Please enter the updated value: ");
+//       const propertyToModify = prompt(
+//         "Please enter the property name you would like to change (title, description, dueDate, priority, status): "
+//       );
+//       if (!taskProperties.includes(propertyToModify))
+//         throw new Error(
+//           `A task does not have that property. Please select from the following:
+//     title, description, dueDate, priority, status`
+//         );
 
-      reconstructedTask.set[propertyToModify](updatedValue);
-      storeTask(reconstructedTask);
-    }
+//       const updatedValue = prompt("Please enter the updated value: ");
 
-    // move task to a different project
-    // todo: potentially to do is to enable user to add the task to another project (as a task can be in multiple, other than in My Tasks), or completely move it to a single project
-    else if (input === "2") {
-      input = prompt(
-        `Would you like to move this task to an existing project? (y / n)`
-      );
+//       reconstructedTask.set[propertyToModify](updatedValue);
+//       storeTask(reconstructedTask);
+//     }
 
-      // move task into existing project
-      if (input === "y") {
-        const selectedProjectName =
-          prompt(`Please type the name of the project to add this task to:
+//     // move task to a different project
+//     // todo: potentially to do is to enable user to add the task to another project (as a task can be in multiple, other than in My Tasks), or completely move it to a single project
+//     else if (input === "2") {
+//       input = prompt(
+//         `Would you like to move this task to an existing project? (y / n)`
+//       );
 
-    Current projects: [ ${retrieveAllProjectNames()} ]`);
-        if (!projectExists(selectedProjectName))
-          throw new Error(`Project, "${selectedProjectName}" does not exist.`);
-        const reconstructedDestinationProject = createProjectFromJSON(
-          retrieveProject(selectedProjectName)
-        );
-        reconstructedDestinationProject.addTasks(retrievedTaskObject);
-        reconstructedDestinationProject.store();
-      }
-      // new project created to move this task to
-      else if (input === "n") {
-        const newProject = createProject(
-          prompt("Please enter a name for your new project")
-        );
-        newProject.addTasks(retrievedTaskObject);
-        newProject.store();
-      }
-      matchedProjects.forEach((retrievedProject) => {
-        const reconstructedProject = createProjectFromJSON(retrievedProject);
-        const taskList = retrievedProject.tasks;
-        const taskListWithoutSelectedTask = taskList.filter(
-          (task) => task.title != selectedTaskName
-        );
-        reconstructedProject.replaceTasks(taskListWithoutSelectedTask);
-        reconstructedProject.store();
-      });
-    }
+//       // move task into existing project
+//       if (input === "y") {
+//         const selectedProjectName =
+//           prompt(`Please type the name of the project to add this task to:
 
-    // delete task
-    else if (input === "3") {
-      matchedProjects.forEach((retrievedProject) => {
-        const reconstructedProject = createProjectFromJSON(retrievedProject);
-        const taskList = retrievedProject.tasks;
-        const taskListWithoutSelectedTask = taskList.filter(
-          (task) => task.title != selectedTaskName
-        );
-        reconstructedProject.replaceTasks(taskListWithoutSelectedTask);
-        reconstructedProject.store();
-      });
-    }
-  } else if (input === "3") {
-    const selectedProjectName = prompt(
-      "Please enter the name of the project you would like to modify:"
-    );
-    if (!projectExists(selectedProjectName))
-      throw new Error(`Project name, "${projectName}", does not exist.`);
-    if (selectedProjectName === "My Tasks")
-      throw new Error(`Cannot modify "My Tasks" project.`);
-    const selectedProject = retrieveProject(selectedProjectName);
+//     Current projects: [ ${retrieveAllProjectNames()} ]`);
+//         if (!projectExists(selectedProjectName))
+//           throw new Error(`Project, "${selectedProjectName}" does not exist.`);
+//         const reconstructedDestinationProject = createProjectFromJSON(
+//           retrieveProject(selectedProjectName)
+//         );
+//         reconstructedDestinationProject.addTasks(retrievedTaskObject);
+//         reconstructedDestinationProject.store();
+//       }
+//       // new project created to move this task to
+//       else if (input === "n") {
+//         const newProject = createProject(
+//           prompt("Please enter a name for your new project")
+//         );
+//         newProject.addTasks(retrievedTaskObject);
+//         newProject.store();
+//       }
+//       matchedProjects.forEach((retrievedProject) => {
+//         const reconstructedProject = createProjectFromJSON(retrievedProject);
+//         const taskList = retrievedProject.tasks;
+//         const taskListWithoutSelectedTask = taskList.filter(
+//           (task) => task.title != selectedTaskName
+//         );
+//         reconstructedProject.replaceTasks(taskListWithoutSelectedTask);
+//         reconstructedProject.store();
+//       });
+//     }
 
-    input = prompt(
-      `What would you like to do with project, "${selectedProjectName}"?
-    1: rename project
-    2: delete project`
-    );
+//     // delete task
+//     else if (input === "3") {
+//       matchedProjects.forEach((retrievedProject) => {
+//         const reconstructedProject = createProjectFromJSON(retrievedProject);
+//         const taskList = retrievedProject.tasks;
+//         const taskListWithoutSelectedTask = taskList.filter(
+//           (task) => task.title != selectedTaskName
+//         );
+//         reconstructedProject.replaceTasks(taskListWithoutSelectedTask);
+//         reconstructedProject.store();
+//       });
+//     }
+//   } else if (input === "3") {
+//     const selectedProjectName = prompt(
+//       "Please enter the name of the project you would like to modify:"
+//     );
+//     if (!projectExists(selectedProjectName))
+//       throw new Error(`Project name, "${projectName}", does not exist.`);
+//     if (selectedProjectName === "My Tasks")
+//       throw new Error(`Cannot modify "My Tasks" project.`);
+//     const selectedProject = retrieveProject(selectedProjectName);
 
-    // rename project
-    if (input === "1") {
-      const newProjectName = prompt(
-        `What should be the new name for your project, "${selectedProjectName}"?`
-      );
-      const renamedProject = createProject(newProjectName);
-      renamedProject.replaceTasks(selectedProject.tasks);
-      renamedProject.store();
-      localStorage.removeItem(selectedProjectName);
-    }
+//     input = prompt(
+//       `What would you like to do with project, "${selectedProjectName}"?
+//     1: rename project
+//     2: delete project`
+//     );
 
-    // delete project
-    else if (input === "2") {
-      const myTasksProject = createProjectFromJSON(retrieveProject("My Tasks"));
+//     // rename project
+//     if (input === "1") {
+//       const newProjectName = prompt(
+//         `What should be the new name for your project, "${selectedProjectName}"?`
+//       );
+//       const renamedProject = createProject(newProjectName);
+//       renamedProject.replaceTasks(selectedProject.tasks);
+//       renamedProject.store();
+//       localStorage.removeItem(selectedProjectName);
+//     }
 
-      myTasksProject.addTasks(selectedProject.tasks);
-      myTasksProject.store();
+//     // delete project
+//     else if (input === "2") {
+//       const myTasksProject = createProjectFromJSON(retrieveProject("My Tasks"));
 
-      localStorage.removeItem(selectedProjectName);
-    }
-  }
+//       myTasksProject.addTasks(selectedProject.tasks);
+//       myTasksProject.store();
 
-  console.log(retrieveAll());
-})();
+//       localStorage.removeItem(selectedProjectName);
+//     }
+//   }
 
-//todo: need to refactor the app here so it's not spaghetti code - package into different functions (methods of an 'app controller')
-//? explore using unique ids of tasks and projects to establish relations rather than manipulating arrays and objects etc... it is quite good to have a unique identifier i think. better performance?
+//   console.log(retrieveAll());
+// })();
+
+// //todo: need to refactor the app here so it's not spaghetti code - package into different functions (methods of an 'app controller')
+// //? explore using unique ids of tasks and projects to establish relations rather than manipulating arrays and objects etc... it is quite good to have a unique identifier i think. better performance?
