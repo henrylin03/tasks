0cb651b35d821eef7f520d3b7ef794be36ccea00
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-05-09 10:43:27 +1000

    remove css minimisers. change order around of rules for webpack config according to asset-management guide of webpack documentation

--- webpack.common.js
+++ webpack.common.js
@@ -1,19 +1,17 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   module: {
     rules: [
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader", MiniCssExtractPlugin.loader],
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
       },
     ],
   },
@@ -21,14 +19,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./src/index.html",
     }),
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css",
-    }),
   ],
-  optimization: {
-    minimizer: [new CssMinimizerPlugin()],
-  },
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
