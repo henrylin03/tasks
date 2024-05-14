c604d04f7fbbae6bb2e02560829157d61a9b7dd2
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-05-14 11:40:45 +1000

    create project div element in DOM. get project names as an array from localStorage using helper function

--- src/js/screenController.js
+++ src/js/screenController.js
@@ -10,6 +10,7 @@ const createScreenController = () => {
   addProjectBtn.addEventListener("click", addProject);
 
   // add anything that needs to be run
+  displayProjects();
 
   return;
 };
@@ -41,12 +42,44 @@ const displayProjects = () => {
     ".current-projects-container"
   );
 
-  const projectNamesArray = app.getProjectNames();
+  const projectNamesArray = app
+    .getProjectNames()
+    .map((p) => createProjectLinkInNav(p));
 
-  alert(projectNamesArray);
+  function createProjectLinkInNav(projectName) {
+    const linkDiv = document.createElement("div");
+    linkDiv.classList.add("link");
+    const linkAnchor = document.createElement("a");
+    linkAnchor.setAttribute("href", "#");
+    const PROJECT_SVG = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                class="icon icon-tabler icons-tabler-outline icon-tabler-list">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M9 6l11 0" />
+                <path d="M9 12l11 0" />
+                <path d="M9 18l11 0" />
+                <path d="M5 6l0 .01" />
+                <path d="M5 12l0 .01" />
+                <path d="M5 18l0 .01" />
+              </svg>`;
+    const projectNameText = document.createElement("p");
+    projectNameText.textContent = projectName;
+
+    linkAnchor.innerHTML = PROJECT_SVG;
+    linkAnchor.appendChild(projectNameText);
+    linkDiv.appendChild(linkAnchor);
+
+    return linkDiv;
+  }
+
+  console.log(projectNamesArray);
+
+  // for each of the project names, generate dom elements and then add it as child to navProjectsContainer. we want to complete remove what's in there though. html will thus have an empty current-projects-container (or not at all if it causes issues with spacing)
+
+  alert(projectNames);
 };
 
 export { createScreenController };
 
 // ? likely will need a factory to create inbox page and a "project" page that then gets populated via js
-//todo: fix errors for project name existing - make those not as thrown errors but actually in the DOM
+//todo: fix errors for project name existing - make those not as thrown errors but actually in the DOM. toggle using js and css class for error (have a look at the signup form project you did)
