88ccddad66647feb42db44713e7423c4ba27b98b
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-05-14 10:25:59 +1000

    write method to get all project names
    at the moment, we don't need all of the project's objects. however, i know we will need this in the future

--- src/js/appController.js
+++ src/js/appController.js
@@ -4,15 +4,18 @@ import { retrieveAllProjectNames } from "./utils/localStorageHelpers";
 const createAppController = () => {
   const addProject = (newProjectName) => createProject(newProjectName).store();
 
-  const getProjects = () => {};
+  const getProjectNames = () =>
+    // we exclude "INBOX", which is a project object
+    retrieveAllProjectNames().filter((projectName) => projectName != "INBOX");
 
   // run
   if (localStorage.length === 0) createProject("INBOX").store();
 
   // WHAT CAN AN APP DO?
-  return { addProject, getProjects };
+  return { addProject, getProjectNames };
 };
 
 export { createAppController };
 
 // NEED TO CREATE MY PROJECTS IF NOT ALREADY THERE! THIS IS THE INBOX
+//todo: prevent creation of an empty-named project
