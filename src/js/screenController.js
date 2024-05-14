ce2231c8ee6657af5f9d0efcd28f7ddc7e485dde
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-05-14 10:38:39 +1000

    add wrapper around current projects' links
    this excludes the title, and the add-project button at the bottom too, to make it easier to manipulate

--- src/js/screenController.js
+++ src/js/screenController.js
@@ -4,18 +4,12 @@ const app = createAppController();
 
 const createScreenController = () => {
   // select required dom elements here
-  const navProjectsContainer = document.querySelector(".projects");
-  console.log(navProjectsContainer);
-
-  // add required event listeners
-
-  // add anything that needs to be run
-
-  const sidebar = document.querySelector(".sidebar");
-
   const addProjectBtn = document.querySelector("#add-project");
 
+  // add required event listeners
   addProjectBtn.addEventListener("click", addProject);
+
+  // add anything that needs to be run
 
   return;
 };
@@ -32,14 +26,27 @@ const addProject = () => {
     app.addProject(input.value);
     form.reset();
     dialog.close();
+    displayProjects();
   }
 
   form.addEventListener("submit", handleSubmit);
-
   cancelBtn.addEventListener("click", () => dialog.close());
 
   // run
   dialog.showModal();
 };
 
+const displayProjects = () => {
+  const navProjectsContainer = document.querySelector(
+    ".current-projects-container"
+  );
+
+  const projectNamesArray = app.getProjectNames();
+
+  alert(projectNamesArray);
+};
+
 export { createScreenController };
+
+// ? likely will need a factory to create inbox page and a "project" page that then gets populated via js
+//todo: fix errors for project name existing - make those not as thrown errors but actually in the DOM
