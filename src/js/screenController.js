75b556d52b2176ee251e19bd7c62341ca053ab80
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-05-15 11:56:00 +1000

    add .icon-container class to figures that wrap svgs in the sidebar. make svgs 100%
    easier to just replace the svg in certain circumstances using js

--- src/js/screenController.js
+++ src/js/screenController.js
@@ -2,7 +2,7 @@ import { createAppController } from "./appController";
 
 const app = createAppController();
 const svgs = {
-  project: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+  project: `<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24" fill="none"
                 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
                 class="icon icon-tabler icons-tabler-outline icon-tabler-list">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
@@ -74,10 +74,13 @@ const displayProjectsInNav = () => {
     linkDiv.classList.add("link");
     const linkAnchor = document.createElement("a");
     linkAnchor.setAttribute("href", "#");
+    const iconContainer = document.createElement("figure");
+    iconContainer.classList.add("icon-container");
     const projectNameText = document.createElement("p");
     projectNameText.textContent = projectName;
 
-    linkAnchor.innerHTML = svgs.project;
+    iconContainer.innerHTML = svgs.project;
+    linkAnchor.appendChild(iconContainer);
     linkAnchor.appendChild(projectNameText);
     linkDiv.appendChild(linkAnchor);
 
