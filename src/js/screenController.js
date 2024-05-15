98107fad965924d61ec6c1bb72a09554fef93bb7
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-05-15 15:43:02 +1000

    clicking branding opens up the inbox page

--- src/js/screenController.js
+++ src/js/screenController.js
@@ -35,18 +35,24 @@ const createScreenController = () => {
 
   // add required event listeners
   addProjectBtn.addEventListener("click", addProject);
-  brandingInSidebar.addEventListener("click", displayProjectPage("Inbox"));
+  brandingInSidebar.addEventListener("click", () => {
+    clearAllSelectedProjectNavLinks();
+    displayProjectPage("Inbox");
+  });
 
   const projectsIncludingInbox = document.querySelectorAll(
     "div[class=link], div.link.selected"
   );
-
   projectsIncludingInbox.forEach((div) =>
     div.addEventListener("click", () => {
-      projectsIncludingInbox.forEach((div) => div.classList.remove("selected"));
+      clearAllSelectedProjectNavLinks();
       displayProjectPage(div.dataset.project);
     })
   );
+
+  function clearAllSelectedProjectNavLinks() {
+    projectsIncludingInbox.forEach((div) => div.classList.remove("selected"));
+  }
 };
 
 const addProject = () => {
