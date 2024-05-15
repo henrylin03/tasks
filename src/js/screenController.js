49f0dfdec0f01d31a2de87517a956d5f711cd2af
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-05-15 11:48:44 +1000

    create lookup object for svgs in screenController. update text content and inner HTML (being the svg) based on the project page that is being loaded

--- src/js/screenController.js
+++ src/js/screenController.js
@@ -1,6 +1,26 @@
 import { createAppController } from "./appController";
 
 const app = createAppController();
+const svgs = {
+  project: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                class="icon icon-tabler icons-tabler-outline icon-tabler-list">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M9 6l11 0" />
+                <path d="M9 12l11 0" />
+                <path d="M9 18l11 0" />
+                <path d="M5 6l0 .01" />
+                <path d="M5 12l0 .01" />
+                <path d="M5 18l0 .01" />
+              </svg>`,
+  inbox: `<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24" fill="none"
+            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+            class="icon icon-tabler icons-tabler-outline icon-tabler-inbox">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" />
+            <path d="M4 13h3l3 3h4l3 -3h3" />
+          </svg>`,
+};
 
 const createScreenController = () => {
   // select required dom elements here
@@ -54,21 +74,10 @@ const displayProjectsInNav = () => {
     linkDiv.classList.add("link");
     const linkAnchor = document.createElement("a");
     linkAnchor.setAttribute("href", "#");
-    const PROJECT_SVG = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                class="icon icon-tabler icons-tabler-outline icon-tabler-list">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <path d="M9 6l11 0" />
-                <path d="M9 12l11 0" />
-                <path d="M9 18l11 0" />
-                <path d="M5 6l0 .01" />
-                <path d="M5 12l0 .01" />
-                <path d="M5 18l0 .01" />
-              </svg>`;
     const projectNameText = document.createElement("p");
     projectNameText.textContent = projectName;
 
-    linkAnchor.innerHTML = PROJECT_SVG;
+    linkAnchor.innerHTML = svgs.project;
     linkAnchor.appendChild(projectNameText);
     linkDiv.appendChild(linkAnchor);
 
@@ -83,7 +92,11 @@ const displayProjectPage = (projectName) => {
   const pageIcon = headerLeftContainer.querySelector("figure");
   const pageTitle = headerLeftContainer.querySelector(".page-title");
 
+  const isInbox = projectName === "Inbox";
+
   // header changes (icon changes too)
+  pageTitle.textContent = projectName;
+  pageIcon.innerHTML = isInbox ? svgs.inbox : svgs.project;
 
   // all tasks are populated (? might need to come back to this later on once i've built tasks in ui)
 
