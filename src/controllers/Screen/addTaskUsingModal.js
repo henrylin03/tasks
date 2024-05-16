c6a59aba389bdf86d8e750820ec5c46a1066a056
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-05-16 14:10:30 +1000

    task urgency is toggled in the UI, changing between outline and filled alert circle

--- src/controllers/Screen/addTaskUsingModal.js
+++ src/controllers/Screen/addTaskUsingModal.js
@@ -1,8 +1,15 @@
 const modal = document.querySelector(".new-task-modal");
+const urgentBtn = document.querySelector(".toggle-urgent-btn");
 
 const addTaskUsingModal = () => {
+  urgentBtn.addEventListener("mousedown", toggleUrgency);
+
   modal.showModal();
   return;
+};
+
+const toggleUrgency = () => {
+  urgentBtn.classList.toggle("is-urgent");
 };
 
 export default addTaskUsingModal;
