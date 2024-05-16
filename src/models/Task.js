abfc255b466701da90821c62534127f290b99dd3
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-05-16 16:24:00 +1000

    rename task name as task name to align with project name (no nameclashing because they're different objects anyway). update setters and details being viewed from task

--- src/models/Task.js
+++ src/models/Task.js
@@ -3,54 +3,34 @@ import { format } from "date-fns";
 const createTask = (name) => {
   let description = "";
   let dueDate = "";
-  let priority = "Medium";
-  let status = "To Do";
+  let urgency = false;
+  let completed = false;
 
   const viewDetails = () => ({
     name,
     description,
     dueDate,
-    priority,
-    status,
+    urgent,
+    completed,
   });
 
-  const setTitle = (newTitle) => (title = newTitle);
+  const setName = (newName) => (name = newName);
   const setDescription = (newDescription) => (description = newDescription);
-
   const setDueDate = (dueDateString) => {
-    if (!dueDateString) return;
-    // user is advised to input due date as format "dd/MM/YYYY" per Australian common use
     const [day, month, year] = dueDateString.split("/");
-
     // month is zero-indexed
     dueDate = format(new Date(year, month - 1, day), "dd/MM/yyyy");
   };
-
-  const setPriority = (newPriority) => {
-    if (!newPriority) return;
-    const VALID_PRIORITIES = ["Low", "Medium", "High", "Urgent"];
-    if (VALID_PRIORITIES.includes(newPriority)) return (priority = newPriority);
-    console.log(
-      "Invalid priority. Reverting to the last defined priority or, if this is a new task, defaulting to 'Medium' priority."
-    );
-  };
-
-  const setStatus = (newStatus) => {
-    if (!newStatus) return;
-    const VALID_STATUSES = ["To Do", "Doing", "Done", "Won't Do"];
-    if (VALID_STATUSES.includes(newStatus)) return (status = newStatus);
-    console.log(
-      "Invalid status. Reverting to the last defined status or, if this is a new task, 'To Do'."
-    );
-  };
+  const toggleUrgency = () => (urgency = !urgency);
+  const toggleCompleted = () => (completed = !completed);
 
   // setters for private properties
   const set = {
-    title: setTitle,
+    name: setName,
     description: setDescription,
     dueDate: setDueDate,
-    priority: setPriority,
-    status: setStatus,
+    urgent: toggleUrgency,
+    completed: toggleCompleted,
   };
 
   return { set, viewDetails };
