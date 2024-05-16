5dcc256257cb1b4a0b62d6d73ede6679dc9f3320
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-05-16 16:50:57 +1000

    select required inputs of form to create new task

--- src/controllers/Screen/addTaskUsingModal.js
+++ src/controllers/Screen/addTaskUsingModal.js
@@ -3,6 +3,9 @@ import { createAppController } from "../App/createAppController";
 const modal = document.querySelector(".new-task-modal");
 const form = document.querySelector(".new-task-modal form");
 const taskNameInput = document.querySelector("#new-task-name");
+const descriptionInput = document.querySelector("#new-task-description");
+const dueDateInput = document.querySelector("#new-task-due-date");
+const taggedProject = document.querySelector("#project-of-new-task");
 const urgentBtn = document.querySelector(".toggle-urgent-btn");
 const cancelBtn = document.querySelector(".new-task-modal .cancel-btn");
 
@@ -20,7 +23,13 @@ const handleSubmit = (e) => {
 
   e.preventDefault();
 
-  app.addTask(taskNameInput.value);
+  // capture all properties required to create task from form
+  //   const newTaskObjectFromForm = {
+  //     name: taskNameInput.value,
+  //     description:
+  //   };
+
+  //   app.addTask(taskNameInput.value);
 };
 
 const toggleUrgency = () => modal.classList.toggle("is-urgent");
