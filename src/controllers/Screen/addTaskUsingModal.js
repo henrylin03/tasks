4c7cc3391a05050ee36921e08dd038e12472d3bf
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-05-16 15:06:25 +1000

    pressing cancel on add task modal closes the modal

--- src/controllers/Screen/addTaskUsingModal.js
+++ src/controllers/Screen/addTaskUsingModal.js
@@ -1,8 +1,11 @@
 const modal = document.querySelector(".new-task-modal");
+const form = document.querySelector(".new-task-modal form");
 const urgentBtn = document.querySelector(".toggle-urgent-btn");
+const cancelBtn = document.querySelector(".new-task-modal .cancel-btn");
 
 const addTaskUsingModal = () => {
   urgentBtn.addEventListener("mousedown", toggleUrgency);
+  cancelBtn.addEventListener("click", () => modal.close());
 
   modal.showModal();
   return;
