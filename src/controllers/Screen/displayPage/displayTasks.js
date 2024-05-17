1a97402679fd23da5b94534b0d3aff81c5cde7ef
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-05-17 11:40:23 +1000

    generate, from the top down, each 'article' that represents each task. completed the checkbox, task name. created unordered list that list items will fall under

--- src/controllers/Screen/displayPage/displayTasks.js
+++ src/controllers/Screen/displayPage/displayTasks.js
@@ -1,3 +1,5 @@
+const tasksContainer = document.querySelector("main .container");
+
 const displayTasks = (project) => {
   // #1: for given project object (argument), get all of the task objects. clear the main section of all cards.
   const taskObjects = project.getTasksAsObjects();
@@ -12,6 +14,33 @@ const displayTasks = (project) => {
 };
 
 const generateTaskDiv = (task) => {
+  const article = document.createElement("article");
+  article.classList.add("task");
+  if (task.urgency) article.classList.add("urgent");
+  if (task.completed) article.classList.add("completed");
+
+  const checkboxDiv = document.createElement("div");
+  checkboxDiv.classList.add("checkbox-container");
+
+  const checkbox = document.createElement("input");
+  checkbox.setAttribute("type", "checkbox");
+  checkbox.classList.add("checkbox");
+
+  const rightDiv = document.createElement("div");
+  rightDiv.classList.add("right");
+
+  const taskName = document.createElement("p");
+  taskName.classList.add("task-name");
+  taskName.textContent = task.getName();
+
+  const taskAttributes = document.createElement("ul");
+  taskAttributes.classList.add("task-attributes");
+
+  checkboxDiv.appendChild(checkbox);
+  rightDiv.appendChild(taskName);
+  article.appendChild(checkboxDiv);
+  article.appendChild(rightDiv);
+
   console.log(task.viewDetails());
 };
 
