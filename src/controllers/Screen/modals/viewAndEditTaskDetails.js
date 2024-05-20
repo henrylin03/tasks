67521315bd90fc0e56313ffbf62ab7e0678fc9b2
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-05-20 10:31:12 +1000

    remove-comments

--- src/controllers/Screen/modals/viewAndEditTaskDetails.js
+++ src/controllers/Screen/modals/viewAndEditTaskDetails.js
@@ -1,9 +1,9 @@
 const modal = document.querySelector(".task-details-modal");
 
 const viewAndEditTaskDetails = (e) => {
-  e.stopPropagation();
-
   modal.showModal();
 };
 
 export default viewAndEditTaskDetails;
+
+//todo: fix issue when user tries to check the checkbox in preview mode, the task pops open
