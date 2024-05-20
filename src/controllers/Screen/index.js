67228886153ca5910c98a847f8c05083703fb65f
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-05-20 10:56:03 +1000

    remove unused comments

--- src/controllers/Screen/index.js
+++ src/controllers/Screen/index.js
@@ -5,17 +5,15 @@ import displayPage from "./displayPage";
 import handleNavLinkClicks from "./nav/handleNavLinkClicks";
 
 const createScreenController = () => {
-  // select required dom elements here
   const brandingInSidebar = document.querySelector(".branding");
   const inboxLinkInSidebar = document.querySelector("#inbox-link");
   const addProjectBtn = document.querySelector(".add-project");
   const addTaskBtn = document.querySelector(".add-task");
 
-  // add anything that needs to be run
   displayProjectsInNav();
   displayPage("Inbox");
 
-  // add required event listeners
+  //event listeners
   addTaskBtn.addEventListener("click", addTaskUsingModal);
   addProjectBtn.addEventListener("click", addProjectUsingModal);
   [brandingInSidebar, inboxLinkInSidebar].forEach((elem) =>
