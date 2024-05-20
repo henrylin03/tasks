e06d4a171cf68f7eb1a45947f0791d97b1af21db
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-05-21 09:29:29 +1000

    display project names in nav. add attributes for id and name
    id is for future-proofing for when we allow for duplicate project names

--- src/controllers/Screen/nav/displayProjectsInNav.js
+++ src/controllers/Screen/nav/displayProjectsInNav.js
@@ -3,47 +3,47 @@ import handleNavLinkClicks from "./handleNavLinkClicks";
 
 const displayProjectsInNav = () => {
   const app = createAppController();
+  const navProjectsContainer = document.querySelector(
+    ".current-projects-container"
+  );
+  const projectsArray = app.getProjects().map((p) => createProjectLinkInNav(p));
 
-  // const navProjectsContainer = document.querySelector(
-  //   ".current-projects-container"
-  // );
+  navProjectsContainer.replaceChildren(...projectsArray);
+};
 
-  // const projectsArray = app
-  //   .getProjects()
-  //   .map((p) => createProjectLinkInNav(p.name));
+const createProjectLinkInNav = (project) => {
+  const projectName = project.name;
+  const projectId = project.id;
 
-  // navProjectsContainer.replaceChildren(...projectsArray);
+  const linkDiv = document.createElement("div");
+  linkDiv.classList.add("link");
+  linkDiv.setAttribute("data-project-name", projectName);
+  linkDiv.setAttribute("data-project-id", projectId);
 
-  // function createProjectLinkInNav(projectName) {
-  //   // create DOM elements from top down
-  //   const linkDiv = document.createElement("div");
-  //   linkDiv.classList.add("link");
-  //   linkDiv.setAttribute("data-project", projectName);
-  //   linkDiv.addEventListener("click", () => handleNavLinkClicks(projectName));
+  linkDiv.addEventListener("click", () => handleNavLinkClicks(projectName));
 
-  //   const linkAnchor = document.createElement("a");
-  //   const iconContainer = document.createElement("figure");
-  //   iconContainer.classList.add("icon-container");
-  //   const projectNameText = document.createElement("p");
-  //   projectNameText.textContent = projectName;
+  const linkAnchor = document.createElement("a");
+  const iconContainer = document.createElement("figure");
+  iconContainer.classList.add("icon-container");
+  const projectNameText = document.createElement("p");
+  projectNameText.textContent = projectName;
 
-  //   iconContainer.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24" fill="none"
-  //               stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-  //               class="icon icon-tabler icons-tabler-outline icon-tabler-list">
-  //               <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-  //               <path d="M9 6l11 0" />
-  //               <path d="M9 12l11 0" />
-  //               <path d="M9 18l11 0" />
-  //               <path d="M5 6l0 .01" />
-  //               <path d="M5 12l0 .01" />
-  //               <path d="M5 18l0 .01" />
-  //             </svg>`;
-  //   linkAnchor.appendChild(iconContainer);
-  //   linkAnchor.appendChild(projectNameText);
-  //   linkDiv.appendChild(linkAnchor);
+  iconContainer.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24" fill="none"
+                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                class="icon icon-tabler icons-tabler-outline icon-tabler-list">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <path d="M9 6l11 0" />
+                <path d="M9 12l11 0" />
+                <path d="M9 18l11 0" />
+                <path d="M5 6l0 .01" />
+                <path d="M5 12l0 .01" />
+                <path d="M5 18l0 .01" />
+              </svg>`;
+  linkAnchor.appendChild(iconContainer);
+  linkAnchor.appendChild(projectNameText);
+  linkDiv.appendChild(linkAnchor);
 
-  //   return linkDiv;
-  return;
+  return linkDiv;
 };
 
 export default displayProjectsInNav;
--- src/index.js
+++ src/index.js
@@ -5,5 +5,3 @@ import createScreenController from "./controllers/Screen";
 // localStorage.clear(); // comment out when you need to reset
 createScreenController();
 console.log(localStorage);
-console.log(JSON.parse(localStorage.getItem("projects")));
-// console.log(Object.keys(JSON.parse(localStorage.getItem("projects"))));
