e64da65a5431c896d8a553c116c75e9fa952b315
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-05-21 16:51:27 +1000

    ensure tasks are rebuilt to include methods in original factory at the appController level

--- src/controllers/App/createAppController.js
+++ src/controllers/App/createAppController.js
@@ -1,5 +1,5 @@
 import { createProject, recreateProjectFromJSON } from "../../models/Project";
-import { createTask } from "../../models/Task";
+import { createTask, recreateTaskFromJSON } from "../../models/Task";
 import {
   retrieveProjectById,
   retrieveProjectByName,
@@ -38,17 +38,31 @@ const createAppController = () => {
   const getProject = (projectId) =>
     recreateProjectFromJSON(retrieveProjectById(projectId));
 
-  const getTask = (taskId) => retrieveTaskById(taskId);
+  const getTask = (taskId) => recreateTaskFromJSON(retrieveTaskById(taskId));
 
-  const modifyTask = (taskIdOrObject) => {
-    const taskObject =
-      typeof task === "string" ? getTask(task) : taskIdOrObject;
+  const updateTask = ({
+    id,
+    name,
+    description,
+    dueDate,
+    urgency,
+    completed,
+    projectName,
+  }) => {
+    const taskBeforeChanging = getTask(id);
+
+    const projectHasChanged = () => {};
+    // get the id for task (we might not need this but probably should just in case)
+    // #1.1: if project has changed, then task's id needs to be popped from the current project's tasks, and added to new project's tasks list
+    // #1.2 then, both projects needs to be pushed back to localStorage
+    // #2: remove task from localStorage
+    // #3: re-store task from localStorage
   };
 
   // run
   if (localStorage.length === 0) createProject("Inbox").store();
 
-  return { addTask, getTask, addProject, getProjects, getProject };
+  return { addTask, getTask, updateTask, addProject, getProjects, getProject };
 };
 
 export { createAppController };
