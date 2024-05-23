2a0bea9e86b95c451573d92cdfa810a22c0addb3
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-05-23 11:31:18 +1000

    generate delete confirmation modal from js i

--- src/controllers/Screen/modals/handleDelete.js
+++ src/controllers/Screen/modals/handleDelete.js
@@ -1,7 +1,41 @@
 const handleDelete = (e) => {
   e.stopPropagation();
-  alert("bin btn clicked");
-  return;
+
+  console.log("bin btn clicked");
+};
+
+const generateModal = (taskObject) => {
+  const taskName = taskObject.getName();
+
+  const modal = document.createElement("dialog");
+  modal.classList.add("delete-modal");
+
+  const form = document.createElement("form");
+
+  const heading = document.createElement("h2");
+  heading.textContent = `"${taskName}" will be permanently deleted.`;
+  const paragraph = document.createElement("p");
+  paragraph.textContent = "You won't be able to undo this action.";
+
+  const buttons = document.createElement("div");
+  buttons.classList.add("btn-group");
+  const cancelBtn = document.createElement("button");
+  cancelBtn.classList.add("cancel-btn");
+  cancelBtn.type = "reset";
+  cancelBtn.textContent = "Cancel";
+  const deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("delete-btn");
+  deleteBtn.type = "button";
+  deleteBtn.textContent = "Delete task";
+
+  buttons.appendChild(cancelBtn);
+  buttons.appendChild(deleteBtn);
+  form.appendChild(heading);
+  form.appendChild(paragraph);
+  form.appendChild(buttons);
+  modal.appendChild(form);
+
+  return modal;
 };
 
 export default handleDelete;
