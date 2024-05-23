70e474132e28b33472616779183f3a37696d5fbe
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-05-23 22:41:46 +1000

    if renamed name is just empty space, or is just the same as old name, then have old name in the title

--- src/controllers/Screen/displayPage/handleProjectNameChange.js
+++ src/controllers/Screen/displayPage/handleProjectNameChange.js
@@ -1,8 +1,24 @@
+import { createAppController } from "../../App/createAppController";
+
+const app = createAppController();
+
 export default function handleProjectNameChange(e) {
-  const newProjectName = e.target.value;
+  const projectId = e.target.dataset.id;
+  const projectObject = app.getProject(projectId);
+  const oldName = projectObject.getName();
 
-  e.target.addEventListener("blur", () => alert("hello"), { once: true });
+  e.target.addEventListener(
+    "blur",
+    (evt) => {
+      const newName = evt.target.value;
 
-  console.log(newProjectName);
+      if (oldName.trim() === newName.trim() || !newName.trim())
+        return (evt.target.value = oldName);
+
+      console.log(oldName.trim() === newName.trim());
+    },
+    { once: true }
+  );
+
   return;
 }
