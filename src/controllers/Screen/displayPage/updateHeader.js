07762819126e77a570b0a9e47f64a6736bc5c790
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2024-05-23 23:41:33 +1000

    Prevent duplicate names in project (#36)
    * add comments into app controller for clarity
    
    * write renameProject method in app controller
    
    * move DOM manipulation of nav from displayPage (which is referring to the main part of the page (<main> element) to the nav side. update paramater names - we are now using id of projects in nav, not the name)
    
    * clear out unnecessary part of html markup  for each page's header that gets populated anyway by js
    
    * simplify updateHeader.js
    
    * reduce font size of header (page title)
    
    * use specific selector for page title
    
    * make the page title have an id (good practice for input fields iiuc), and populate the value in updateHeader when you click onto a new project page
    
    * make page title bold
    
    * rename class of page icon. remove box shadow on focus
    
    * adjust font colour, add border when input field is focused. once clicked, all text is highlighted
    
    highlighting all text is copied from Microsoft Tasks. it just allows the user to delete the whole project name more easily, which is a nice shortcut!
    
    * adjust padding of input field. reduce font size of heading. adjust margin and gap so that there's alignment with task below
    
    * project name can take up the whole space between the icon to the left and the 'add a task' anchor link to the right
    
    covers for when the project name is super long
    
    * update selector to be more specific and self-documenting
    
    * move event listener into js
    
    there will be other event listeners too - easier to have all in one place for maintainability
    
    * disable header change if it is inbox page
    
    * on click of the page title for editing, the icon is hidden
    
    * once blurred (clicked outside of the input field), the icon returns
    
    * prevent renaming of inbox
    
    * remove unused code / comment out for now
    
    * add id of the project page the user is looking at as a data attribute to the #page-title
    
    * add onclick event highlighting all text of project name in html
    
    * have separate module for handler for project name changing
    
    * remove try/catch in adding project modal because we now enable duplicate names (suffix get added). add module in helpers/ folder to check for duplicate project names and add suffix if required
    
    * update variable names in module for adding suffix to duplicate project names
    
    * update events to mousedown
    
    see john carmack
    
    * add event listener to blur of project name change
    
    * only run blur event handler once
    
    can always remount it once user clicks the input again
    
    * if renamed name is just empty space, or is just the same as old name, then have old name in the title
    
    * add suffix to the renamed project name if it is a duplicate by calling the helper
    
    * set the name, add it to the handler for project name change, then store into localStorage
    
    * remove console.log statements
    
    * ensure correct use of cleaned project names rather than just unclean project names
    
    * remove console log statements
    
    * return cleaned project name if it is not in storage
    
    * remove unused return statement

--- src/controllers/Screen/displayPage/updateHeader.js
+++ src/controllers/Screen/displayPage/updateHeader.js
@@ -1,6 +1,8 @@
-//todo: make svg imported - there is too much markup here...
-const SVGS = {
-  project: `<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24" fill="none"
+import handleProjectNameChange from "./handleProjectNameChange";
+
+const updateHeader = (project) => {
+  const SVGS = {
+    project: `<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24" fill="none"
                 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
                 class="icon icon-tabler icons-tabler-outline icon-tabler-list">
                 <path stroke="none" d="M0 0h24v24H0z" fill="none" />
@@ -11,25 +13,24 @@ const SVGS = {
                 <path d="M5 12l0 .01" />
                 <path d="M5 18l0 .01" />
               </svg>`,
-  inbox: `<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24" fill="none"
+    inbox: `<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24" fill="none"
             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
             class="icon icon-tabler icons-tabler-outline icon-tabler-inbox">
             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
             <path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" />
             <path d="M4 13h3l3 3h4l3 -3h3" />
           </svg>`,
-};
+  };
+  const pageIcon = document.querySelector(".page-icon");
+  const pageTitle = document.querySelector("#page-title");
 
-const updateHeader = (project) => {
-  const projectId = project.getId();
+  const currentProjectPageId = project.getId();
+  const isInboxPage = currentProjectPageId === "inbox";
 
-  const pageIcon = document.querySelector("header .left figure");
-  const pageTitle = document.querySelector(".page-title");
-  const navbarLink = document.querySelector(`[data-id="${projectId}"]`);
-
-  navbarLink.classList.add("selected");
-  pageTitle.textContent = project.getName();
-  pageIcon.innerHTML = projectId === "inbox" ? SVGS.inbox : SVGS.project;
+  pageTitle.value = project.getName();
+  pageTitle.setAttribute("data-id", currentProjectPageId);
+  pageTitle.disabled = isInboxPage;
+  pageIcon.innerHTML = isInboxPage ? SVGS.inbox : SVGS.project;
 };
 
 export default updateHeader;
