19e1907619811e73b8cb611f740cb4a923e66952
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-05-23 22:36:39 +1000

    add event listener to blur of project name change

--- src/controllers/Screen/displayPage/handleProjectNameChange.js
+++ src/controllers/Screen/displayPage/handleProjectNameChange.js
@@ -1,3 +1,8 @@
-export default function handleProjectNameChange() {
+export default function handleProjectNameChange(e) {
+  const newProjectName = e.target.value;
+
+  e.target.addEventListener("blur", () => alert("hello"));
+
+  console.log(newProjectName);
   return;
 }
--- src/controllers/Screen/index.js
+++ src/controllers/Screen/index.js
@@ -3,17 +3,20 @@ import addProjectUsingModal from "./modals/addProjectUsingModal";
 import displayProjectsInNav from "./nav/displayProjectsInNav";
 import displayPage from "./displayPage";
 import handleNavLinkClicks from "./nav/handleNavLinkClicks";
+import handleProjectNameChange from "./displayPage/handleProjectNameChange";
 
 const createScreenController = () => {
   const brandingInSidebar = document.querySelector(".branding");
   const inboxLinkInSidebar = document.querySelector("#inbox-link");
   const addProjectBtn = document.querySelector(".add-project");
   const addTaskBtn = document.querySelector(".add-task");
+  const pageTitle = document.querySelector("#page-title");
 
   displayProjectsInNav();
   displayPage("inbox");
 
   //event listeners
+  pageTitle.addEventListener("mousedown", handleProjectNameChange);
   addTaskBtn.addEventListener("mousedown", addTaskUsingModal);
   addProjectBtn.addEventListener("mousedown", addProjectUsingModal);
   [brandingInSidebar, inboxLinkInSidebar].forEach((elem) =>
