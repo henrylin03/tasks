210f603a33dbf9b8e522de7408e57ffa32cac12c
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-05-24 10:51:19 +1000

    add bin btn to projects (but can't see them yet)

--- src/controllers/Screen/nav/displayProjectsInNav.js
+++ src/controllers/Screen/nav/displayProjectsInNav.js
@@ -29,6 +29,9 @@ const createProjectLinkInNav = (project) => {
   iconContainer.classList.add("icon-container");
   const projectNameText = document.createElement("p");
   projectNameText.textContent = projectName;
+  const binBtn = document.createElement("button");
+  binBtn.type = "button";
+  binBtn.classList.add("delete-icon-btn");
 
   iconContainer.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 24 24" fill="none"
                 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
@@ -41,11 +44,10 @@ const createProjectLinkInNav = (project) => {
                 <path d="M5 12l0 .01" />
                 <path d="M5 18l0 .01" />
               </svg>`;
-
   linkAnchor.appendChild(iconContainer);
   linkAnchor.appendChild(projectNameText);
+  linkAnchor.appendChild(binBtn);
   linkDiv.appendChild(linkAnchor);
-
   navProjectsContainer.appendChild(linkDiv);
 };
 
