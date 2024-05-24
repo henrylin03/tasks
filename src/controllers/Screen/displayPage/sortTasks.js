61f5c1ddb1a6bd7232a9e89502d7dec2cb9577e6
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-05-24 15:59:44 +1000

    combine arrays with spread operator

--- src/controllers/Screen/displayPage/sortTasks.js
+++ src/controllers/Screen/displayPage/sortTasks.js
@@ -1,15 +1,12 @@
 export default function sortTasks(taskObjectsArray) {
   if (taskObjectsArray.length === 0) return taskObjectsArray;
 
-  // isolate the tasks that do have a due date
   const tasksWithDueDates = taskObjectsArray.filter((t) => t.getDueDate());
   tasksWithDueDates.sort(
     (taskA, taskB) =>
       new Date(taskA.getDueDate()) - new Date(taskB.getDueDate())
   );
-  //   tasksWithDueDates.forEach((t) => console.log(t.viewDetails()));
 
-  // then, for tasks that don't have a due date, we sort by creation date (proxied by their ids)
   const tasksWithoutDueDates = taskObjectsArray.filter(
     (t) => t.getDueDate() === ""
   );
@@ -18,16 +15,6 @@ export default function sortTasks(taskObjectsArray) {
       Number(taskA.getId().substring(1)) - Number(taskB.getId().substring(1))
   );
 
-  console.log(tasksWithoutDueDates[0].viewDetails());
-
-  // use spread operator to combine them
-
-  //   const sortedArray = taskObjectsArray.sort((a, b) => {
-  //     console.log(new Date(a.getDueDate()), new Date(b.getDueDate()));
-  //     return a - b;
-  //   });
-
+  return [...tasksWithDueDates, ...tasksWithoutDueDates];
   //  ! ensure sorting is done after every modification of every task detail too
-
-  return tasksWithoutDueDates; // but sorted
 }
