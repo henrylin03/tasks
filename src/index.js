7b00185cdaf05482b6ca75b53894eb85952cd5a5
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-05-24 16:49:24 +1000

    remove console log statement

--- src/index.js
+++ src/index.js
@@ -2,6 +2,4 @@ import "./assets/styles/normalise.css";
 import "./assets/styles/global.css";
 import createScreenController from "./controllers/Screen";
 
-// localStorage.clear(); // comment out when you need to reset
 createScreenController();
-console.log(localStorage);
