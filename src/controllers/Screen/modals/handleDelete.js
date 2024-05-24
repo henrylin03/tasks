2e9d5fe0f18b515513b5f1fa87dd9c7d8a2ff6c8
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-05-24 11:42:46 +1000

    try and generalise handleDelete.js to both task and object deletions

--- src/controllers/Screen/modals/handleDelete.js
+++ src/controllers/Screen/modals/handleDelete.js
@@ -2,23 +2,34 @@ import { createAppController } from "../../App/createAppController";
 import displayPage from "../displayPage";
 import { closeModal } from "./modalHandlers";
 
-const handleDelete = (taskObject) => {
-  const modal = generateModal(taskObject);
+const app = createAppController();
+
+const handleDelete = (taskOrProjectObj) => {
+  const modal = generateModal(taskOrProjectObj);
   modal.showModal();
 };
 
-const generateModal = (taskObject) => {
-  const taskName = taskObject.getName();
+const generateModal = (obj) => {
+  const id = obj.getId();
+  const name = obj.getName();
+  const type = Array.from(id)[0] === "T" ? "task" : "project";
 
   const modal = document.createElement("dialog");
   modal.classList.add("delete-modal");
-
   const form = document.createElement("form");
-
   const heading = document.createElement("h2");
-  heading.textContent = `"${taskName}" will be permanently deleted.`;
+  heading.textContent = `"${name}" will be permanently deleted.`;
+  form.appendChild(heading);
   const paragraph = document.createElement("p");
   paragraph.textContent = "You won't be able to undo this action.";
+  form.appendChild(paragraph);
+
+  if (type === "project") {
+    const taskMoveParagraph = document.createElement("p");
+    taskMoveParagraph.textContent =
+      "All of its tasks will move into your Inbox.";
+    form.appendChild(taskMoveParagraph);
+  }
 
   const buttons = document.createElement("div");
   buttons.classList.add("btn-group");
@@ -26,25 +37,25 @@ const generateModal = (taskObject) => {
   cancelBtn.classList.add("cancel-btn");
   cancelBtn.type = "reset";
   cancelBtn.textContent = "Cancel";
-  cancelBtn.addEventListener("mousedown", () => closeModal(modal));
   const deleteBtn = document.createElement("button");
   deleteBtn.classList.add("delete-btn");
   deleteBtn.type = "button";
-  deleteBtn.textContent = "Delete task";
+  deleteBtn.textContent = `Delete ${type}`;
+
+  buttons.appendChild(cancelBtn);
+  buttons.appendChild(deleteBtn);
+  form.appendChild(buttons);
+  modal.appendChild(form);
+  document.body.appendChild(modal);
+
+  // add event listeners
   deleteBtn.addEventListener("mousedown", () => {
-    createAppController().deleteTask(taskObject);
+    app.deleteTask(taskObject);
     closeModal(modal);
     displayPage(taskObject.getProjectId());
   });
 
-  buttons.appendChild(cancelBtn);
-  buttons.appendChild(deleteBtn);
-  form.appendChild(heading);
-  form.appendChild(paragraph);
-  form.appendChild(buttons);
-  modal.appendChild(form);
-
-  document.body.appendChild(modal);
+  cancelBtn.addEventListener("mousedown", () => closeModal(modal));
 
   return modal;
 };
