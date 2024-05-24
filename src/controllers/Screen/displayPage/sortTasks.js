2fe9f1b529641224a833ab6247beb516dfbc5d8e
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-05-24 16:08:16 +1000

    write comparison functions outside for reusability. find completed tasks

--- src/controllers/Screen/displayPage/sortTasks.js
+++ src/controllers/Screen/displayPage/sortTasks.js
@@ -1,6 +1,9 @@
 export default function sortTasks(taskObjectsArray) {
   if (taskObjectsArray.length === 0) return taskObjectsArray;
 
+  const completedTasks = taskObjectsArray.filter((t) => t.getCompleted());
+  //   return completedTasks;
+
   const tasksWithDueDates = taskObjectsArray.filter((t) => t.getDueDate());
   tasksWithDueDates.sort(
     (taskA, taskB) =>
@@ -17,3 +20,9 @@ export default function sortTasks(taskObjectsArray) {
 
   return [...tasksWithDueDates, ...tasksWithoutDueDates];
 }
+
+const earlierDueDateComesFirst = (taskA, taskB) =>
+  new Date(taskA.getDueDate()) - new Date(taskB.getDueDate());
+
+const earlierCreationDateComesFirst = (taskA, taskB) =>
+  Number(taskA.getId().substring(1)) - Number(taskB.getId().substring(1));
